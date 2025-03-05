c860c78878346bd5cf81c5d12e99821c1652ea02
HSSN42 <hello@hssn.art>
2025-03-05 20:25:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const fs = require('fs');
-
-// Import routes
-const authRoutes = require('./src/routes/authRoutes');
-const adminRoutes = require('./src/routes/adminRoutes');
-const publicRoutes = require('./src/routes/publicRoutes');
+const multer = require('multer');
 
 // Initialize Express app
 const app = express();
@@ -25,8 +21,33 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Create src/routes directory if it doesn't exist
+const routesDir = path.join(__dirname, 'src', 'routes');
+if (!fs.existsSync(routesDir)) {
+  fs.mkdirSync(routesDir, { recursive: true });
+}
+
+// Create src/utils directory if it doesn't exist
+const utilsDir = path.join(__dirname, 'src', 'utils');
+if (!fs.existsSync(utilsDir)) {
+  fs.mkdirSync(utilsDir, { recursive: true });
+}
+
+// Create src/middleware directory if it doesn't exist
+const middlewareDir = path.join(__dirname, 'src', 'middleware');
+if (!fs.existsSync(middlewareDir)) {
+  fs.mkdirSync(middlewareDir, { recursive: true });
+}
+
+// Import routes
+const authRoutes = require('./src/routes/authRoutes');
+const adminRoutes = require('./src/routes/adminRoutes');
+const publicRoutes = require('./src/routes/publicRoutes');
+
 // Static files for React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+if (fs.existsSync(path.join(__dirname, 'client/build'))) {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+}
 
 // API Routes
 app.use('/api/auth', authRoutes);
@@ -39,9 +60,11 @@ app.get('/api/health', (req, res) => {
 });
 
 // Serve React app for all other routes
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
+if (fs.existsSync(path.join(__dirname, 'client/build'))) {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 // Error handling middleware
 app.use((err, req, res, next) => {
